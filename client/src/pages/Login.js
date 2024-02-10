c0bbfaa94aae2e95625920518be9a8370e296697
seangorelik <seangor19@gmail.com>
2024-02-10 00:15:37 -0600

    implemented login page and started work on the backend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,25 @@
 import Navbar from "../components/NavBar.js";
-import "./Login.css"
+import "./Login.css";
+import { useState } from 'react';
 
 function Login () {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <>
             <Navbar/>
-            <div>
-                
+            <div id="signInBackground">
+                <form id="signInForm">
+                    <img id="logo" alt="logo" src={require("../images/NewPollLogo.png")}/>
+                    <input className="signInInputs" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}/>
+                    <input className="signInInputs" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                    <button type="submit" id="signInButton">Sign In</button>
+                </form>
             </div>
         </>
     
     );
 }
+
 export default Login;
